218c2afe46fa278d550646d7936b7fd8455535d2
Naila Nur <23naila.n@gmail.com>
2017-06-21 17:46:16 -0400

    first commit

---
+++ popup.js
@@ -0,0 +1,27 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function onSubmit(e) {
+  var rant = document.querySelector('#rb-rant').value;
+  console.log(rant);
+  tags = tags.split(' ');
+}
+
+function onTypeTag(e) {
+  console.log(e);
+  var tags = document.querySelector('#rb-tags').value;
+}
+
+$('#rb-tags').tagsInput({
+  height:'50px',
+  width:'380px',
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var button = document.querySelector('#rb-submit');
+  var tags = document.querySelector('#rb-tags');
+  button.addEventListener('click', onSubmit);
+  tags.addEventListener('keyup', onTypeTag);
+});
